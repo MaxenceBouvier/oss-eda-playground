47c67991c142e32353516f0a14ce536386890f8a
Maxence Bouvier <maxence.bouvier.pro@gmail.com>
2025-08-07 11:55:23 +0000

    Add WorkSpace Verilog Config

---
+++ resources/design/source/multiplier.sv
@@ -0,0 +1,13 @@
+
+module multiplier #(
+    parameter N_IN = 4,
+    parameter N_OUT = 8
+    ) (
+    input logic [N_IN-1:0] operand_a_i,
+    input logic [N_IN-1:0] operand_b_i,
+    output logic [N_OUT-1:0] result_o
+);
+
+assign result_o
+
+endmodule
