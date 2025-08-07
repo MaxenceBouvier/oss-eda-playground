4f22497e9cddd43b3e8454b34ee2864b7b41119b
Maxence Bouvier <maxence.bouvier.pro@gmail.com>
2025-08-07 12:55:52 +0200

    First Commit Functional Dockerfile

---
+++ .devcontainer/.setup/manual_build.sh
@@ -0,0 +1,3 @@
+
+docker build -t oss_eda_playground:latest $SRC_DIR/.devcontainer
+
---
+++ .devcontainer/.setup/setup_nvidia_toolkit.sh
@@ -0,0 +1,18 @@
+# Optional: Update NVIDIA Drivers
+sudo apt update
+sudo apt upgrade
+apt search nvidia-driver # Search for latest versions
+sudo apt install nvidia-driver-750 nvidia-dkms-750
+sudo reboot
+
+# Setup NVIDIA Container Toolkit
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+
